c7ace10032c55316709670e20e109c4cd44e0b83
Ted Brunell <tbrunell@redhat.com>
2021-06-09 09:20:50 -0400

    bug fix

--- HowsMySalute.py
+++ HowsMySalute.py
@@ -37,8 +37,8 @@ flat = ""
 extension = ""
 
 # For webcam input:
-cap = cv2.VideoCapture(1)  # The video capture device (1) may need to be changed for your system.
-with mp_holistic.Holistic(model_complexity=1,smooth_landmarks="true",min_detection_confidence=0.6,min_tracking_confidence=0.6) as holistic:
+cap = cv2.VideoCapture(0)  # The video capture device (1) may need to be changed for your system.
+with mp_holistic.Holistic(smooth_landmarks="true",min_detection_confidence=0.6,min_tracking_confidence=0.6) as holistic:
 
     while cap.isOpened():
         ret, frame = cap.read()
