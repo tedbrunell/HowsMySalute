399c00efa54ad1e976dc72154b13945e8fae5275
Ted Brunell <tbrunell@redhat.com>
2021-06-09 20:20:05 -0400

    Updated to use flask for browser based use

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, Response
+from HowsMySalute import salute
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def gen(HowsMySalute):
+    while True:
+        frame = HowsMySalute.get_salute()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    # print("done")
+    return Response(gen(salute()),mimetype='multipart/x-mixed-replace; boundary=frame')
+
+
+if __name__ == '__main__':
+    app.run()
+    app.debug = True
